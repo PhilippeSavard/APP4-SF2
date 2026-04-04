509ad36c4dbfb52ab0c76a65970ba91156e3240a
PhilippeSavard <2542496@cegeplimoilou.ca>
2026-04-04 17:35:43 -0400

    Implémentation de constructeur de Circuit Builder

--- src/app/CircuitBuilder.java
+++ src/app/CircuitBuilder.java
@@ -1,4 +1,11 @@
 package app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CircuitBuilder {
+    private final ObjectMapper mapper;
+
+    public CircuitBuilder() {
+        this.mapper = new ObjectMapper();
+    }
 }
