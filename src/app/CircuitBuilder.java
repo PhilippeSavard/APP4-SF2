71b455c53e3f2f692a32fc553b313c522684c7a0
PhilippeSavard <2542496@cegeplimoilou.ca>
2026-04-04 17:36:57 -0400

    Implémentation de commentaire

--- src/app/CircuitBuilder.java
+++ src/app/CircuitBuilder.java
@@ -3,7 +3,7 @@ package app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CircuitBuilder {
-    private final ObjectMapper mapper;
+    private final ObjectMapper mapper; // ObjectMapper = utile pour lecture de JsonNode
 
     public CircuitBuilder() {
         this.mapper = new ObjectMapper();
