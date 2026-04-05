f85ca9ab01ab7bb7640f84023eb33e1a55a292ed
PhilippeSavard <2542496@cegeplimoilou.ca>
2026-04-04 22:48:24 -0400

    Implémentation de commentaires jugée important et de un commentaire personelle comme un to do task.

--- src/app/CircuitApp.java
+++ src/app/CircuitApp.java
@@ -21,23 +21,23 @@ public class CircuitApp {
 
         boolean pasFini = true; //tant que ce n'est pas finit alors il faut continuer l'application.
 
-        while (pasFini){
-            List<File> fichierJson = obtenirFichiersJson(dossier);
+        while (pasFini){ // tant que tu n'a pas fini, continue.
+            List<File> fichierJson = obtenirFichiersJson(dossier); // obtien les fichiers json du fichier dossier.
 
             if (fichierJson.isEmpty()){ //l'array list de tout les fichiers à l'intérieur du fichier dossier est vide.
                 System.out.println("Aucun fichier JSON à été trouvé dans le dossier fichiers_json.");
                 return; //pour arrêter la méthode
             }
-            System.out.println("\nFichiers JSON disponibles :");
+            System.out.println("\nFichiers JSON disponibles :"); // présente les fichiers disponibles s'il existe.
             for (int index = 0; index < fichierJson.size(); index++){
                 System.out.println("[" + (index + 1) + "]" + fichierJson.get(index).getName());
             }
 
-            int choix = 0;
+            int choix = 0; // initialiser tout comme choix valide
             boolean choixValide = false;
 
-            while(!choixValide){
-                System.out.println("Entrez le numéro du fichier : ");
+            while(!choixValide){ // vus que initialiser false alors il vas l'éxecuter au début
+                System.out.println("Entrez le numéro du fichier : "); // section pour aller chercher le fichier voulu de l'utilisateur.
                 String entreeUtilisateur = scanner.nextLine().trim();
 
                 try {
@@ -51,26 +51,26 @@ public class CircuitApp {
                 }
             }
 
-            File fichierChoisi = fichierJson.get(choix - 1);
+            File fichierChoisi = fichierJson.get(choix - 1); // logique de index de java qui commence à 0 au lieu de 1.
 
             try{
-                Composant circuit = CircuitGlobale.construireCircuit(fichierChoisi.getPath());
-                double resistance = circuit.calculerResistance();
+                Composant circuit = CircuitGlobale.construireCircuit(fichierChoisi.getPath()); // construit le circuit
+                double resistance = circuit.calculerResistance(); // calcule la résistance du circuit
                 System.out.printf("Résistance équivalente calculée : %.2f Ω%n", resistance); // un print qui précise la manière dont tu veux qu'il imprime.
                 // %f = affiche un nombre décimal, .2 = garde 2 chiffre après la virgule, Ω = ohm l'unité, %n = va à la ligne de manière propre. tout cela chercher sur google.
                 // si une explication est nécessaire cela me fera un plaisir de le faire.
-            } catch (IOException e){
+            } catch (IOException e){ // dans le cas ou c'est illisible
                 System.out.println("Impossible de lire le fichier JSON.");
-            }catch (IllegalArgumentException e){
+            }catch (IllegalArgumentException e){ // dans le cas ou le fichier JSON contient des données invalides.
                 System.out.println("Le fichier JSON contient des données invalides : " + e.getMessage());
             }
-            pasFini = demanderNouvelleAction(scanner);
+            pasFini = demanderNouvelleAction(scanner); // si le programme s'arrête ou non dépend de demanderNouvelleAction qui demande avec le R et le Q.
         }
 
-        System.out.println("Fermeture de l'application.");
+        System.out.println("Fermeture de l'application."); // arrête l'application une fois que l'utilisateur appui sur Q voir ligne 67.
     }
 
-    private List<File> obtenirFichiersJson(File dossier) {
+    private List<File> obtenirFichiersJson(File dossier) { // ****** Je ne comprend pas pourquoi il ne marche pas c'est ce qui fait que j'avance pas live.
         List<File> fichiersJson = new ArrayList<>();
 
         File[] tousLesFichiersJson = dossier.listFiles();
@@ -86,7 +86,7 @@ public class CircuitApp {
         }
         return fichiersJson;
     }
-    private boolean demanderNouvelleAction(Scanner scanner){
+    private boolean demanderNouvelleAction(Scanner scanner){ // demande une nouvelle action npour changer ou non la valeur de pasFini dans éxécuter.
         while(true){
             System.out.println("\n[R] Tester un autre fichier | [Q] Quitter : "); // question
             String entreeUtilisateur = scanner.nextLine().trim().toUpperCase(); // passe une ligne enlève les espace inutile et met le en majuscule.
